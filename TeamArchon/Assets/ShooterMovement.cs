4e7643830cafd968203970f585e1101b87477b67
MAIN\bcs4985 <bcs4985@rit.edu>
2018-09-21 11:52:33 -0400

    Started work on 2d Shooter

---
+++ TeamArchon/Assets/ShooterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterMovement : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle-90, Vector3.forward);
+
+        
+
+        gameObject.transform.position += Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)) * speed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gameObject.GetComponent<TwoDimensionWeapon>().Fire();
+        }
+
+        
+	}
+}
